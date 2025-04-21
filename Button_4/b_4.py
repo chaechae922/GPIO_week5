dbd7fdc323816e46d0923df5ed3df20c05e99fec
chaechae922 <121761183+chaechae922@users.noreply.github.com>
2025-04-21 16:21:20 +0900

    Create b_4.py

---
+++ Button_4/b_4.py
@@ -0,0 +1,17 @@
+from gpiozero import LEDBoard, Button
+from signal import pause
+
+leds = LEDBoard(20, 16, 7, 8, pwm=False)
+button = Button(25, pull_up=True)
+
+i = 0
+
+def counter():
+    global i
+    print(i)
+    leds.value = [(i >> bit) & 1 for bit in range(4)]
+    i = (i + 1) % 16
+
+button.when_pressed = counter
+
+pause()
